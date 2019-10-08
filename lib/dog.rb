b00ef1f761825a5f9f4e24b9146c97c04f6070de
Amra Sezairi <amra.sezairi@hotmail.com>
2019-10-08 19:20:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,22 +3,6 @@ class Dog
     @name=name
     @breed =breed
   end
-  
-  def name
-    @name
-  end
-  
-  def name=(new_name)
-    
-    @name = new_name
-  end
-  
-  def breed 
-    @breed
-  end
-  
-  def breed=(new_breed)
-    @breed = new_breed
-  end
 end
-fido = Dog.new
+
+
