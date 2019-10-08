6d02fd04f07cf0b6a1735b30c196cddbd1061889
Amra Sezairi <amra.sezairi@hotmail.com>
2019-10-08 19:13:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,15 @@
 class Dog 
-  def initialize(name)
+  def initialize(name,breed)
     @name=name
+    @breed=breed
   end
   
-  def 
+  def name
+    @name
+  end
+  
+  def name=(new_name)
+    @name=new_name
+  end
+  
 end
